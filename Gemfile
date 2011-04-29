25b5fb6b5ca5f55e7d99bf964c719cd48cef2d4b
John Bintz <john@coswellproductions.com>
2011-04-29 10:31:50 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'feedzirra'
+gem 'curb'
+gem 'sqlite3'
+
+group :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+
+  gem 'autotest'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealSimpleSlurper::Application.config.secret_token = 'f021fbb6533e67cc340e396cc332b0ee955a4cb7cfbd6a8b07483bf7ba6b89c7f97b98853e29d7fd214ea51c33d5976896861751548f8c21d0d18d8d43cafac3'
